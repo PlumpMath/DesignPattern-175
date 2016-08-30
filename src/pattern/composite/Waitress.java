944f4bedf40c69045143ab60878c3a2942f72f65
ritobrotos <rito.rcks@gmail.com>
2016-08-30 19:28:04 +0530

    Composite Pattern

---
+++ src/pattern/composite/Waitress.java
@@ -0,0 +1,14 @@
+package pattern.composite;
+
+public class Waitress {
+
+	MenuComponent allMenu;
+	
+	public Waitress(MenuComponent allMenu){
+		this.allMenu = allMenu;
+	}
+	
+	public void printMenu(){
+		allMenu.print();
+	}
+}
