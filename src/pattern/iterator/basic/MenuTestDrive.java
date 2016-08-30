3f89500a3908a9826e3070fd370f1c5287c3385d
ritobrotos <rito.rcks@gmail.com>
2016-08-30 13:31:52 +0530

    Basic Level Of Iterator PAttern

---
+++ src/pattern/iterator/basic/MenuTestDrive.java
@@ -0,0 +1,13 @@
+package pattern.iterator.basic;
+
+public class MenuTestDrive {
+
+	public static void main(String args[]){
+		PancakeHouseMenu pancakeMenu = new PancakeHouseMenu();
+		DinerMenu dinerMenu = new DinerMenu();
+		
+		Waitress waitress = new Waitress(pancakeMenu, dinerMenu);
+		waitress.printMenu();
+	}
+	
+}
